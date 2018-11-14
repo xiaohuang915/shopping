f4f216bf0b420ac0de4dfc6c81027da422361a73
huangpengcheng <huangpengcheng@jew315.com>
2018-11-14 16:34:01 +0800

    增加配置中心

---
+++ shopping-config/src/main/java/com/springcloud/shopping/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.springcloud.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Auther: pc.huang
+ * @Date: 2018/11/14 15:41
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
