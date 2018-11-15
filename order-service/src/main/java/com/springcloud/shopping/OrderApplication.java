ffac01fa02e1924061ef3a748b50bc67f9bf0f04
huangpengcheng <huangpengcheng@jew315.com>
2018-11-15 12:47:12 +0800

    bus测试

--- order-service/src/main/java/com/springcloud/shopping/OrderApplication.java
+++ order-service/src/main/java/com/springcloud/shopping/OrderApplication.java
@@ -2,6 +2,7 @@ package com.springcloud.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class OrderApplication {
     public static void main(String[] args){
         SpringApplication.run(OrderApplication.class,args);
