8405f7173dd394386c931a7e2ffb331a13e7d950
huangpengcheng <huangpengcheng@jew315.com>
2018-11-15 13:42:52 +0800

    文件修改

--- order-service-double/src/main/java/com/springcloud/shopping/controller/OrderDoubleController.java
+++ order-service-double/src/main/java/com/springcloud/shopping/controller/OrderDoubleController.java
@@ -1,5 +1,7 @@
 package com.springcloud.shopping.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/order")
+@RefreshScope
 public class OrderDoubleController {
+    @Value("${test}")
+    private String test;
+
     @GetMapping("/getOrder/{id}")
     public String getOrder(@PathVariable String id) {
-        return "orderDouble-user==" + id;
+        return "orderDouble-user==" + id + test;
     }
 }
